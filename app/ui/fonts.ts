4d179f3691abd4375d8d6d785b1ad8208e9f4eae
pendragon <mquirossuarez6@gmail.com>
2025-11-30 19:38:40 -0600

    Add Lusitana font to the font imports

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
-import { Inter, Nunito } from 'next/font/google';
+import { Inter, Nunito, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] }); 
 export const nunito = Nunito({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: '400', subsets: ['latin'] });
